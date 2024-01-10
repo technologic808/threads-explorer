87bd2b78efed316ebf20c51f46ea0ac15db267fe
technologic808 <20627603+technologic808@users.noreply.github.com>
2024-01-11 01:51:21 +0530

    Add redirects to prevent null data display

--- app/group/[id]/page.js
+++ app/group/[id]/page.js
@@ -1,10 +1,15 @@
 import { fetchThreads } from '../../api/useThreads'
-
+import { redirect } from 'next/navigation'
 import HomePage from '../../components/HomePage'
 import '../../home.css'
 
 export default async function GroupDetails({ params }) {
+
+  if (!params.id || params.id === 'undefined') { redirect('/') }
+
   const threads = await fetchThreads(params.id)
+
+  if (!threads || threads.length === 0) { redirect('/') }
 
   return <>
     <div className='headline'>{threads.length} threads to explore</div>

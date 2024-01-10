ae2f7fba44f43757870b19d7787cbcb9e14bb823
technologic808 <20627603+technologic808@users.noreply.github.com>
2024-01-11 01:53:31 +0530

    Redirect user for better UX

--- app/group/page.js
+++ app/group/page.js
@@ -1,7 +1,9 @@
 import Link from 'next/link';  // Import the Link component
+import { redirect } from 'next/navigation'
 import '../home.css'
 
 export default async function Home() {
+  redirect('/')
   return <>
     <div className='headline'> Group home page. Add /groupID in url to see group details.</div>
     <p><b>Example: </b></p>

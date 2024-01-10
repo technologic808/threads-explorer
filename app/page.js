5d345eff0daf11f18b6bcec10257520051a3fec1
technologic808 <20627603+technologic808@users.noreply.github.com>
2024-01-11 01:53:06 +0530

    Add Homepage for Threads Explorer

--- app/page.js
+++ app/page.js
@@ -1,13 +1,43 @@
-import { fetchThreads } from './api/useThreads'
-import HomePage from './components/HomePage'
+'use client'
+import { useState } from 'react';
+import { useRouter } from 'next/navigation'
 import './home.css'
+import { Button, TextField, Typography } from '@mui/material';
+import { Add, KeyboardReturn } from '@mui/icons-material';
 
-export default async function Home() {
+export default function Home() {
+  const [groupCode, setGroupCode] = useState('');
+  const router = useRouter()
 
-  const threads = await fetchThreads("azure_spr")
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    router.push(`/group/${groupCode}`);
+  };
 
-  return <>
-    <div className='headline'>{threads.length} threads to explore</div>
-    <HomePage threads={threads} />
-  </>
-}          
+  return (
+    <>
+      <div className='headline'>♾️ threads to explore</div>
+      <div>
+        <Typography className='header' variant="body1" gutterBottom margin={3}>
+          Already have a group code?
+        </Typography>
+        <TextField
+          className='form-element'
+          value={groupCode}
+          onChange={(e) => setGroupCode(e.target.value)}
+          onKeyDown={(e) => { if (e.key === 'Enter') { handleSubmit(e) } }}
+          placeholder="Enter group code"
+        />
+        <Button className='form-element submit-button' variant="contained" onClick={handleSubmit} ><KeyboardReturn /></Button>
+      </div>
+
+      <Typography className='header' variant="body1" gutterBottom margin={3}>
+        Get Started with Threads Explorer
+      </Typography>
+      <div className='form-element'>
+        <Button startIcon={<Add />} variant="contained" onClick={() => router.push('/group/create')}>Create a Group</Button>
+      </div>
+
+    </>
+  );
+}

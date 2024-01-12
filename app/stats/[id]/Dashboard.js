62eb9d42bcb5b2d4ba7e1d015bb65ddf3d1a4143
technologic808 <20627603+technologic808@users.noreply.github.com>
2024-01-12 10:51:37 +0530

    Update User Cards feature
    v2.0
    
    Add relevant information in a user readable format
    Add emojis and styling for cards
    Compose card layout

--- app/stats/[id]/Dashboard.js
+++ app/stats/[id]/Dashboard.js
@@ -1,7 +1,101 @@
 'use client'
-import { Card, CardContent, Typography, Avatar, List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import { Card, CardContent, Typography, Avatar, List, ListItem, ListItemIcon, ListItemText, Container } from "@mui/material";
 import { BarChart } from "@mui/x-charts";
+import { Box, ThemeProvider, createTheme } from '@mui/system';
 import './dashboard.css'
+import { Calculate, ChildFriendly, Elderly, EventAvailable, Functions, SquareFoot } from "@mui/icons-material";
+
+const theme = createTheme({
+    palette: {
+        background: {
+            paper: '#fff',
+        },
+        text: {
+            primary: '#173A5E',
+            secondary: '#46505A',
+        },
+        action: {
+            active: '#001E3C',
+        },
+        success: {
+            dark: '#009688',
+        },
+        failure: {
+            dark: '#E53935',
+        },
+    },
+});
+
+function UserProfileCard({ user, aggregateData }) {
+    // Calculate percentage differences
+    const percentTotalMessages = (user.totalMessages / aggregateData.totalMessages) * 100;
+
+    function describeUserActivity(userActiveDays, totalDays) {
+        const ratio = userActiveDays / totalDays;
+
+        if (ratio === 1) {
+            return "every day";
+        } else if (ratio >= 0.5) {
+            return "every other day";
+        } else if (ratio >= 1 / 3 && ratio < 0.5) {
+            return "every two to three days";
+        } else if (ratio >= 1 / 5 && ratio < 1 / 3) {
+            return "every three to five days";
+        } else if (ratio < 1 / 5) {
+            return "less than once a week";
+        } else {
+            return "invalid input";
+        }
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Box
+                sx={{
+                    bgcolor: 'background.paper',
+                    boxShadow: 1,
+                    borderRadius: 2,
+                    p: 2,
+                    minWidth: 300,
+                }}
+            >
+                <Box sx={{ color: 'text.secondary' }}>{user.name}</Box>
+                <Box sx={{ color: 'text.primary', fontSize: 34, fontWeight: 'medium' }}>
+                    {user.totalMessages} / {aggregateData.totalMessages} messages
+                </Box>
+                <Box
+                    sx={{
+                        color: 'success.dark',
+                        display: 'inline',
+                        fontWeight: 'bold',
+                        mx: 0.5,
+                        fontSize: 14,
+                    }}
+                >
+                    {percentTotalMessages.toFixed(2)}%
+                </Box>
+                <Box sx={{ color: 'text.secondary', display: 'inline', fontSize: 14 }}>
+                    of total messages sent.
+                </Box>
+                <br />
+                <Box sx={{ color: 'text.secondary', display: 'inline', fontSize: 14 }}>
+                    Group activity :
+                </Box>
+                <Box
+                    sx={{
+                        color: 'success.dark',
+                        display: 'inline',
+                        fontWeight: 'bold',
+                        mx: 0.5,
+                        fontSize: 14,
+                    }}
+                >
+                    {describeUserActivity(user.activeDays, aggregateData.daysBetween)}
+                </Box>
+            </Box>
+        </ThemeProvider>
+    );
+}
 
 function Dashboard({ dashboardData }) {
 
@@ -18,6 +112,7 @@ function Dashboard({ dashboardData }) {
     const longestMessageData = dashboardData.LongestMessage.map(user => ({ id: user.user_id, label: user.username, value: user.longest_message }));
     const activeDaysData = dashboardData.ActiveDays.map(user => ({ id: user.user_id, label: user.username, value: user.active_days }));
 
+    // Data for all charts
     const chartsData = [{
         label: 'Total Messages',
         data: totalMessagesData,
@@ -42,6 +137,14 @@ function Dashboard({ dashboardData }) {
         emoji: "📅",
         color: '#76b7b2'
     }];
+
+    // Create aggregate data for comparison. e.g total messages in the group, etc. 
+    const aggregateData = {
+        totalMessages: totalMessagesData.reduce((acc, curr) => acc + curr.value, 0),
+        averageMessageLength: averageMessageLengthData.reduce((acc, curr) => acc + curr.value, 0) / averageMessageLengthData.length,
+        longestMessage: longestMessageData.reduce((acc, curr) => acc.value > curr.value ? acc : curr).value,
+        activeDays: activeDaysData.reduce((acc, curr) => acc + curr.value, 0),
+    };
 
     // Helper function to render BarCharts
     const renderBarChart = (data, title, emoji, color) => (
@@ -70,7 +173,7 @@ function Dashboard({ dashboardData }) {
         <>
             <Typography variant='h1' style={{ "margin": "20px", background: "yellow" }}>Stats Explorer</Typography>
             {renderCharts()}
-            <UserCards data={transformData(dashboardData)} />
+            <UserCards data={transformData(dashboardData)} aggregateData={aggregateData} />
         </>
     );
 }
@@ -113,10 +216,19 @@ const getInitials = (name) => {
 };
 
 
-function UserCards({ data }) {
+function UserCards({ data, aggregateData }) {
+
+    // calculate number of days between earliest first message and latest last message among all users
+    // iterate between the dates of all users to find the earliest and latest date
+    const firstMessageDate = new Date(data.reduce((acc, curr) => acc < curr.firstMessageDate ? acc : curr.firstMessageDate, data[0].firstMessageDate));
+    const lastMessageDate = new Date(data.reduce((acc, curr) => acc > curr.lastMessageDate ? acc : curr.lastMessageDate, data[0].lastMessageDate));
+    const daysBetween = Math.ceil((lastMessageDate - firstMessageDate) / (1000 * 60 * 60 * 24));
+    aggregateData.daysBetween = daysBetween;
+
+
     return (
         <div style={{ backgroundColor: "lightseagreen" }}>
-            <Typography variant='h1' style={{ "margin": "20px" }}>🃏 User Cards</Typography>
+            <Typography variant='h1' style={{ padding: "30px" }}>🃏 User Cards</Typography>
 
             {data.map((user) => {
                 const firstMessageDate = new Date(user.firstMessageDate).toLocaleString("en-US", {
@@ -136,31 +248,31 @@ function UserCards({ data }) {
                 return (
                     <Card elevation={10} variant="outlined" style={{ "margin": "10px", backgroundColor: "yellow" }} key={user.name} className="">
                         <CardContent>
-                            <Avatar>{getInitials(user.name)}</Avatar>
-                            <Typography variant="h6">{user.name}</Typography>
+
+                            <UserProfileCard user={user} aggregateData={aggregateData} />
                             <List className="">
                                 <ListItem>
-                                    <ListItemIcon>{/* First message icon */}</ListItemIcon>
+                                    <ListItemIcon><ChildFriendly /></ListItemIcon>
                                     <ListItemText primary={`First Message: ${firstMessageDate}`} />
                                 </ListItem>
                                 <ListItem>
-                                    <ListItemIcon>{/* Last message icon */}</ListItemIcon>
+                                    <ListItemIcon><Elderly /></ListItemIcon>
                                     <ListItemText primary={`Last Message: ${lastMessageDate}`} />
                                 </ListItem>
                                 <ListItem>
-                                    <ListItemIcon>{/* Total messages icon */}</ListItemIcon>
+                                    <ListItemIcon><Functions /></ListItemIcon>
                                     <ListItemText primary={`Total Messages: ${user.totalMessages}`} />
                                 </ListItem>
                                 <ListItem>
-                                    <ListItemIcon>{/* Average message length icon */}</ListItemIcon>
+                                    <ListItemIcon><Calculate /></ListItemIcon>
                                     <ListItemText primary={`Average Message Length: ${user.avgMessageLength}`} />
                                 </ListItem>
                                 <ListItem>
-                                    <ListItemIcon>{/* Longest message icon */}</ListItemIcon>
+                                    <ListItemIcon><SquareFoot /></ListItemIcon>
                                     <ListItemText primary={`Longest Message: ${user.longestMessage}`} />
                                 </ListItem>
                                 <ListItem>
-                                    <ListItemIcon>{/* Active days icon */}</ListItemIcon>
+                                    <ListItemIcon><EventAvailable /></ListItemIcon>
                                     <ListItemText primary={`Active Days: ${user.activeDays}`} />
                                 </ListItem>
                             </List>
